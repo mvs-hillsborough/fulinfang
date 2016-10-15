1944cbcaf960a6205177c738a25002406344c839
liwei18 <81248241@qq.com>
2016-10-15 14:20:45 +1300

    first for fulinfang

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function section1(){
+        return view('section1');
+    }
+
+    public function layout(){
+        return view('layout');
+    }
+}
